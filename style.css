f96718b1a2e3bfe0be63ac4b30bd62bfffa4dbdf
Eddudu <carloseduardof80@gmail.com>
2026-09-02 17:49:38 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="night-sky"></div>
+    <script src="script.js"></script>
+</body>
+</html>
