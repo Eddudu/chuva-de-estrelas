87fc0f992ac24e407cdce17b83d994b12268036b
Eddudu <carloseduardof80@gmail.com>
2026-09-02 20:02:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nightSky = document.getElementById('night-sky');
 const numberOfStars = 25;
+const colors = ['lightseagreen', 'deepskyblue', 'violet', 'white', 'gold'];
 
 for (let i = 0; i < numberOfStars; i++) {
   const star = document.createElement('div');
@@ -10,12 +11,14 @@ for (let i = 0; i < numberOfStars; i++) {
   const delay = Math.random() * 5;
   const top = Math.random() * 50 - 20;
   const left = Math.random() * 120 - 10;
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
   star.style.setProperty('--size', size);
   star.style.setProperty('--duration', `${duration}s`);
   star.style.setProperty('--delay', `${delay}s`);
   star.style.setProperty('--top', `${top}vh`);
   star.style.setProperty('--left', `${left}vw`);
+  star.style.setProperty('--star-color', randomColor);
 
   nightSky.appendChild(star);
 }
